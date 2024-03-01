a9bd2316ec011ddf0a1e1a7c070fc98889cd2358
Ángela Fouz Suárez <91564188+Angie7kat@users.noreply.github.com>
2024-03-01 09:30:09 +0100

    Comentarios

--- P1/static_list.c
+++ P1/static_list.c
@@ -4,112 +4,125 @@
  * AUTHOR 1: Ángela Fouz Suárez LOGIN 1: angela.fouz
  * AUTHOR 2: Nolan Duteil       LOGIN 2: nolan.duteil
  * GROUP: 3.1
- * DATE: ** / ** / **
+ * DATE: 02 / 03 / 2024
  */
 #include "static_list.h"
 #include <string.h>
 
 void createEmptyList(tList* L){
     L->lastPos = LNULL;
-    /*Asignamos un valor nulo a la ultima posicion que hemos creado*/
+    /* Asignamos un valor nulo a la última posición para crear una lista vacía.*/
 }
 
 bool isEmptyList(tList L){
     return L.lastPos == LNULL;
-    /*A partir del valor de la ultima posicion creada, devuelve verdadero si es un valor nulo, y falso si tiene un valor no nulo*/
+    /* Según el valor de la última posición de la lista, devuelve verdadero si es un valor nulo(la lista está vacía),
+     * y falso si tiene un valor no nulo.*/
 }
 
 tPosL first(tList L){
     return 0;
-    /*Devuelve el numero de la primera posicion en la lista*/
+    /* Devolvemos la primera posición de la lista.*/
 }
 
 tPosL last(tList L){
     return L.lastPos;
-    /*Devuelve el numero de la ultima posicion en la lista*/
+    /* Devolvemos el número de la última posición de la lista.*/
 }
 
 tPosL next(tPosL p, tList L){
-    if(p==L.lastPos)
+    if(p==L.lastPos){
         return LNULL;
-    /*Creamos un condicional que determina si el valor dado es el ultimo de la lista. Si es asi devuelve un valor nulo*/
-    else
+        /* Creamos un condicional que determina si la posición dada es la última de la lista; si es así devuelve un
+         * valor nulo...*/
+    }else{
         return ++p;
-    /*En caso de que no sea el ultimo valor creado, se aumenta en 1 el valor dado*/
+        /* ... En caso de que no sea la última posición de la lista, se aumenta 1 la posición para acceder al siguiente
+         * elemento.*/
+    }
 }
 
 tPosL previous(tPosL p, tList L){
-    if(p==0)
+    if(p==0){
         return LNULL;
-    /*Creamos un condicional que determina si el valor dado es el primero de la lista. Si es asi devuelve un valor nulo*/
-    else
+        /* Creamos un condicional que determina si la posición dada es la primera de la lista. Si es asi devuelve un
+         * valor nulo*/
+    }else{
         return --p;
-    /*A diferencia de la funcion next, si el valor el primero de la lista, se disminuye en 1 el valor dado*/
+        /* A diferencia de la función next, debemos devolver el anterior por lo que se disminuye en 1 la posición de la
+         * lista para acceder a la anterior.*/
+    }
 }
-bool insertItem (tItemL Item, tPosL p, tList* L) {
+bool insertItem(tItemL Item, tPosL p, tList* L){
     tPosL q;
-    /*Creamos una variable auxiliar para recorrer el bucle*/
-    if (L->lastPos == MAX - 1)
+    /* Creamos una variable auxiliar para recorrer el bucle.*/
+    if(L->lastPos == MAX - 1){
         return false;
-    /*Creamos un condicional para comprobar si el valor dado puede insertarse en la posicion dada.
-     *En caso de que la lista este llena, devuelve falso*/
-    else{
+        /* Creamos un condicional para comprobar si el valor puede insertarse en la posición indicada.
+        *En caso de que la lista este llena, devuelve falso.*/
+    }else{
         L->lastPos++;
-        /*Si la lista no esta llena, aumentamos en 1 el valor de la ultima posicion*/
-        if(p == LNULL)
+        /* Si la lista no esta llena, aumentamos en 1 el valor de la última posición.*/
+        if(p == LNULL){
             L->Item[L->lastPos] = Item;
-            /*Creamos otro condicional para saber si el valor de p es nulo y posteriormente insertar el Item dado*/
-        else {
-            for(q = L->lastPos; q>p; q--)
-                /*En caso de no ser nulo, creamos un bucle para recorrer la lista desde la ultima posicion creada*/
+            /* Creamos otro condicional para saber si el valor de la posición introducida es nulo y posteriormente
+             * insertar el Item que se guardará en la última posición de la lista.*/
+        }else{
+            for(q = L->lastPos; q>p; q--){
+                /* En caso de no ser nulo la posición introducida, creamos un bucle para recorrer la lista buscando la
+                 * posición indicada.*/
                 L->Item[q] = L->Item[q-1];
-                /*A cada posicion recorrida por la lista le asignamos el valor de la posicion anterior a ella*/
-                L->Item[q] = Item;
-                /*Finalmente */
+                /* En la última posición(que está vacía) duplicamos la información de la anterior y así hasta llegar a
+                 * la posición en la que queremos insertar el Item.*/
+            }
+            L->Item[q] = Item;
+            /* Insertamos el Item en esa posición.*/
         }
         return true;
     }
 }
 void deleteAtPosition(tPosL p, tList* L){
     tPosL q;
-    /*Creamos una variable auxiliar para recorrer un bucle*/
+    /* Creamos una variable auxiliar para recorrer un bucle.*/
     for(q=p; q<L->lastPos; q++){
-        /*Creamos una lista que a partir del valor dado, y mientras el valor sea
-         * menor que la ultima posicion creada, aumentamos en 1 su valor*/
         L->Item[q] = L->Item[q+1];
-        /*A partir del valor dado, asignamos al valor por el que
-         * pasa la lista el valor siguiente, para asi eliminar el valor asignado a la posicion dada*/
+        /* En el bucle para borrar el Item de esa posición, nos posicionamos en ella y copiamos el elemento de la
+         * siguiente posición en este para así eliminarlo.
+         * Ahora tenemos en mismo Item en dos posiciones y para eso el bucle va haciendo lo mismo con el resto de
+         * posiciones; así el elemento queda eliminado y la última posición y la anterior con el mismo elemento, por
+         * lo que ... */
     }
     L->lastPos--;
-    /*Finalmente disminuimos en 1 el valor del tamano de la lista*/
+    /* ... Lo siguiente que hacemos es eliminar la última posición y así obtenemos la lista sin el elemento de
+     * la posición deseada y sin ningún elemento duplicado. */
 }
 
 tItemL getItem(tPosL p, tList L){
     return L.Item[p];
-    /*Devuelve el contenido en la posicion dada*/
+    /* Devolvemos el contenido que hay en la posición dada.*/
 }
 
 void updateItem(tItemL Item, tPosL p, tList* L){
     L->Item[p] = Item;
-    /*Cambiamos el contenido de la posicion dada por otro valor dado*/
+    /* Cambiamos el contenido de la posición por el valor indicado.*/
 }
 
 tPosL findItem(tUserName name, tList L){
     tPosL p;
-    /*Creamos una variable auxiliar para recorrer un bucle*/
+    /* Creamos una variable auxiliar para recorrer un bucle.*/
     if(L.lastPos == LNULL){
         return LNULL;
-        /*Creamos un condicional que devuelve nulo si la ultima posicion creada tiene valor nulo*/
+        /* Creamos un condicional que devuelve nulo si la lista está vacía. */
     }else{
-        for(p = 0; p <= L.lastPos; ++p){
-            /*En caso de que no sea nulo, creamos un bucle desde 0 hasta la ultima posicion creada*/
+        for (p = 0; p <= L.lastPos; ++p){
+            /* En caso de que no esté vacía, creamos un bucle desde 0 hasta la última posición de la lista.*/
             if (strcmp(name, L.Item[p].userName) == 0) {
                 return p;
-                /*Finalmente creamos un condicional que se asegura de que el contenido
-                 * del nombre en la posicion del bucle sea igual al nombre dado*/
+                /* Finalmente creamos un condicional que busca el nombre del user asegurándose de que el contenido
+                 * de la posición del bucle sea igual al nombre dado.*/
             }
         }
         return LNULL;
-        /*En caso de que no coincida con ninguna posicion, devuelve nulo*/
+        /* Si no lo encuentra devuelve nulo.*/
     }
 }
