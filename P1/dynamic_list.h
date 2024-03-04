9e9333474e2c9b0002d3c00559b496b90f9bd7cd
Ángela Fouz Suárez <91564188+Angie7kat@users.noreply.github.com>
2024-03-04 17:34:37 +0100

    Dinámicas
    He preparado las funciones de la dinámica para que el jueves sea más fácil aunque a lo mejor hay que ir empezando antes viendo que tendremos 4 días para hacerlo así q a lo mejor mañana sigo.

--- P1/dynamic_list.h
+++ P1/dynamic_list.h
@@ -1,17 +1,118 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * AUTHOR 1: Ángela Fouz Suárez LOGIN 1: angela.fouz
+ * AUTHOR 2: Nolan Duteil       LOGIN 2: nolan.duteil
+ * GROUP: 3.1
  * DATE: ** / ** / **
  */
 
 #ifndef DYNAMIC_LIST_H
 #define DYNAMIC_LIST_H
+#include <stdbool.h>
+#define LNULL NULL
 
 #include "types.h"
 
-/* Write your code here... */
+typedef int tItemL;
+typedef struct tNode* tPosL;
+struct tNodo{
+    tItemL data;
+    tPosL next;
+};
+typedef tPosL tList;
+
+void createEmptyList(tList* L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+bool isEmptyList(tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tPosL first(tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tPosL last(tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tPosL next(tPosL p, tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tPosL previous(tPosL p, tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+bool insertItem(tItemL d, tPosL p, tList* L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+void deleteAtPosition(tPosL p, tList * L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tItemL getItem(tPosL p, tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+void updateItem(tItemL d, tPosL p, tList * L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+tPosL findItem(tItemL d, tList L);
+/*
+ * Objetivo:
+ * Entradas:
+ * Salidas:
+ * PreCD:
+ * PostCD:
+ */
+
+// Estas se definen en teoría y NO salen en el enunciado
+bool copyList(tList L, tList * M);
+void deleteList(tList * L);
 
 #endif
