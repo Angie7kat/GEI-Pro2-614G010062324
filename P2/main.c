57eb71266aee4e0c5723ba9aba16146eeef62c4b
Ángela Fouz Suárez <91564188+Angie7kat@users.noreply.github.com>
2024-04-18 09:29:16 +0200

    Add files via upload

--- P2/main.c
+++ P2/main.c
@@ -4,7 +4,7 @@
  * AUTHOR 1: Ángela Fouz Suárez LOGIN 1: angela.fouz
  * AUTHOR 2: Nolan Duteil       LOGIN 2: nolan.duteil
  * GROUP: 3.1
- * DATE: ** / ** / **
+ * DATE: 18 / 04 / 2024
  */
 
 #include <stdio.h>
@@ -40,11 +40,11 @@ tUserCategory changeTypeToEnum(char * category){
 
 /* Función auxiliar que nos ayudará a calcular el promedio en la función stats ya que los valores a dividir son int
  * y queremos que sea float.*/
-float average (int category, int plays) {
-    if (category == 0 || plays == 0)
+float average (int category, int plays){
+    if(category == 0 || plays == 0)
         return 0;
         /* Si los valores ingresados son 0, devuelve el valor 0.*/
-    else {
+    else{
         return ((float)plays / (float)category);
         /* De lo contrario devuelve el promedio.*/
     }
@@ -58,20 +58,28 @@ float average (int category, int plays) {
  * PostCD: Las posiciones de los usuarios pueden haber variado.
  */
 void new(tListU *L, tUserName name, tUserCategory category){
-    tItemU item1;
-
     tPosU pos;
     pos = findItemU(name, *L);
-
+    /* Creamos una variable auxiliar tPosU para comprobar si el usuario ya está en la lista.*/
     if(pos == NULLU){
-        strcpy(item1.userName, name);
-        item1.totalPlayTime = 0;
-        item1.userCategory = category;
-        createEmptyListS(&item1.songList);
-        insertItemU(item1, L);
-        printf("* New: user %s category %s\n", name, changeTypeToChar(category));
+        /* Si no está en la lista entonces...*/
+        tItemU usuario;
+        strcpy(usuario.userName, name);
+        usuario.totalPlayTime = 0;
+        usuario.userCategory = category;
+        createEmptyListS(&usuario.songList);
+        /* ... Creamos un tItemU usuario al que le asignaremos toda la información introducida.*/
+        if(insertItemU(usuario, L)) {
+            /* Si es insertado en la lista correctamente...*/
+            printf("* New: user %s category %s\n", name, changeTypeToChar(category));
+            /* ... Mostramos en pantalla sus datos.*/
+        }else{
+            /* Si no ha sido insertado correctamente devolvemos error.*/
+            printf("+ Error: New not possible\n");
+        }
     }else{
         printf("+ Error: New not possible\n");
+        /* Si el usuario ya está en la lista devuelve error.*/
     }
 }
 /*    DELETE
@@ -90,27 +98,35 @@ void new(tListU *L, tUserName name, tUserCategory category){
  * PreCD: La lista de usuarios y de canciones debe estar inicializada.
  * PostCD: Las posiciones de las canciones pueden haber variado.
  */
-void add(tListU *L, tUserName name, tSongTitle song) {
-    if(isEmptyListU(*L))
+void add(tListU *L, tUserName name, tSongTitle song){
+    /* Si la lista no tiene ningún usuario no podemos añadir la canción a ningún usuario.*/
+    if(isEmptyListU(*L)){
         printf("+ Error: Add not possible\n");
-    else if(findItemU(name, *L) == NULLU)
+    }else if(findItemU(name, *L) == NULLU){
+        /* Si no encuentra el usuario donde se quiere aumentar la lista de canciones devuelve error.*/
         printf("+ Error: Add not possible\n");
-    else {
+    }else{
         tItemU usuario = getItemU(findItemU(name, *L), *L);
-        if(findItemS(song, usuario.songList) != NULLS)
+        /* Una vez pasados los filtros usaremos un tItemU para obtener todos los datos del usuario que quiere introducir
+         * la canción y miraremos si la canción ya está añadida o no.*/
+        if(findItemS(song, usuario.songList) != NULLS){
+            /* Si la canción ya está en la lista de canciones mostramos un error.*/
             printf("+ Error: Add not possible\n");
-        else{
-            tItemS item;
-            strcpy(item.songTitle, song);
-            item.playTime = 0;
-            if(!insertItemS(item, NULLS, &usuario.songList))
+        }else{
+            tItemS cancion;
+            strcpy(cancion.songTitle, song);
+            cancion.playTime = 0;
+            /* Si la canción no está en la lista de canciones debemos insertarla entonces usamos un tItemS para
+             * introducir los datos de la canción en la lista.*/
+            if(!insertItemS(cancion, NULLS, &usuario.songList)){
+                /* Si no es capaz de insertarla devolvemos un error.*/
                 printf("+ Error: Add not possible\n");
-            else{
+            }else{
                 updateItemU(usuario,findItemU(name, *L),L);
+                /* Si ha sido insertada correctamente actualizamos el usuario, ya que hemos insertado una nueva canción
+                 * en la lista de canciones de un usuario y mostraremos por pantalla el usuario y la canción añadida.*/
                 printf("* Add: user %s adds song %s\n", name, song);
             }
-
-
         }
     }
 }
@@ -140,48 +156,49 @@ void add(tListU *L, tUserName name, tSongTitle song) {
 void stats(tListU L){
     tPosU i;
     tPosS u;
-    /* Creamos una variable auxiliar para recorrer el bucle.*/
-    if(isEmptyListU(L)) {
+    /* Creamos dos variables auxiliares para recorrer los bucles.*/
+    if(isEmptyListU(L)){
         printf("+ Error: Stats not possible\n");
-        /* Si la lista está vacía, muestra error.*/
-    }
-    else {
+        /* Si la lista de usuarios está vacía, muestra error, no hay nada que imprimir.*/
+    }else{
         int cntCategory0 = 0, cntCategory1 = 0, cntTotalPlay0 = 0, cntTotalPlay1 = 0;
         /* Si no está vacía, creamos variables para contar los diferentes categorías.*/
         i = firstU(L);
-        while (i != NULLU) {
+        while (i != NULLU){
             /* Creamos un bucle desde la primera posición de la lista hasta la última.*/
             tItemU Usuario = getItemU(i, L);
             /* Recopilamos los datos del usuario asignado a cada posición.*/
-            if(Usuario.userCategory == 0) {
+            if(Usuario.userCategory == 0){
                 cntCategory0++;
                 cntTotalPlay0 += Usuario.totalPlayTime;
-                /* En caso de ser de la categoría basic, añadimos 1 al contador basic y sumamos las reproducciones de
+                /* En caso de ser de la categoría basic, añadimos 1 al contador basic y sumamos el totalPlayTime de
                  * su categoría.*/
-            }
-            else {
+            }else{
                 cntCategory1++;
                 cntTotalPlay1 += Usuario.totalPlayTime;
-                /* Si pertenece a la categoría pro, añadimos 1 al contador pro y sumamos las reproducciones de su
+                /* Si pertenece a la categoría pro, añadimos 1 al contador pro y sumamos el totalPlayTime de su
                  * categoría.*/
             }
             printf("User %s category %s totalplaytime %d\n",Usuario.userName, changeTypeToChar(Usuario.userCategory), Usuario.totalPlayTime);
+            /* Mostramos la información del usuario por pantalla y ahora nos disponemos a imprimir sus canciones:*/
             if(isEmptyListS(Usuario.songList)){
+                /* Si la lista de canciones está vacía no hay, imprimimos No songs.*/
                 printf("No songs\n");
-            }else {
+            }else{
+                /* Si sí hay canciones, una o varias, debemos recorrer la lista de canciones e ir imprimiendo la
+                 * información de cada una.*/
                 for(u = firstS(Usuario.songList); u != NULLS; u = nextS(u,Usuario.songList)){
                     printf("Song %s playtime %d\n", getItemS(u, Usuario.songList).songTitle, Usuario.totalPlayTime);
                 }
             }
             printf("\n");
-            /* Imprimimos por pantalla los datos de cada usuario.*/
             i = nextU(i, L);
         }
         printf("Category  Users  TimePlay  Average\n");
         printf("Basic     %5d %9d %8.2f\n", cntCategory0, cntTotalPlay0, average(cntCategory0, cntTotalPlay0));
         printf("Pro       %5d %9d %8.2f\n", cntCategory1, cntTotalPlay1, average(cntCategory1, cntTotalPlay1));
         /* Finalmente creamos una tabla para mostrar la información de cada categoría con el número de usuarios,
-         * reproducciones y el promedio.*/
+         * tiempo total de reproducción y el promedio.*/
     }
 }
 /*    REMOVE
--- P2/song_list.c
+++ P2/song_list.c
@@ -4,7 +4,7 @@
  * AUTHOR 1: Ángela Fouz Suárez LOGIN 1: angela.fouz
  * AUTHOR 2: Nolan Duteil       LOGIN 2: nolan.duteil
  * GROUP: 3.1
- * DATE: 11 / 04 / 2024
+ * DATE: 18 / 04 / 2024
  */
 
 #include "song_list.h"
